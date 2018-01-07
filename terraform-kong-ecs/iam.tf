82f63149e44e6813f787e858a2e263408de28229
HaraShun <forest.hara0702@outlook.com>
2018-01-07 21:27:51 +0900

    add comments and modify bug about  IAM role on iam.tf
    add comments and modify bug about  IAM role on iam.tf

--- terraform-kong-ecs/iam.tf
+++ terraform-kong-ecs/iam.tf
@@ -1,3 +1,6 @@
+###########
+# IAM Role
+###########
 resource "aws_iam_role" "ecs_instance_role" {
   name               = "ecs_instance_role"
   assume_role_policy = "${file("policies/ec2-assume-role.json")}"
@@ -13,26 +16,30 @@ resource "aws_iam_role" "ecs_autoscale_role" {
   assume_role_policy = "${file("policies/autoscale-assume-role.json")}"
 }
 
-resource "aws_iam_policy_attachment" "ecs_instance_role_attach" {
-  name       = "ecs-instance-role-attach"
-  roles      = ["${aws_iam_role.ecs_instance_role.name}"]
+
+########################
+# IAM Policy Attachment
+########################
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
+  role      = "${aws_iam_role.ecs_instance_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_policy_attachment" "ecs_service_role_attach" {
-  name       = "ecs-service-role-attach"
-  roles      = ["${aws_iam_role.ecs_service_role.name}"]
+resource "aws_iam_role_policy_attachment" "ecs_service_role_attach" {
+  role      = "${aws_iam_role.ecs_service_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
-resource "aws_iam_policy_attachment" "ecs_autoscale_role_attach" {
-  name       = "ecs-autoscale-role-attach"
-  roles      = ["${aws_iam_role.ecs_autoscale_role.name}"]
+resource "aws_iam_role_policy_attachment" "ecs_autoscale_role_attach" {
+  role      = "${aws_iam_role.ecs_autoscale_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
 }
 
+#######################
+# IAM Instance Profile
+#######################
 resource "aws_iam_instance_profile" "ecs" {
-  name  = "ecs-instance-profile"
+  name = "ecs-instance-profile"
   path  = "/"
   role = "${aws_iam_role.ecs_instance_role.name}"
 }
