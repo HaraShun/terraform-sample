c32612e037e749a68dd850664ba6358256979953
HaraShun <forest.hara0702@outlook.com>
2018-09-03 05:21:40 +0900

    delete terraform version and provider

--- terraform-kong-ecs/config.tf
+++ terraform-kong-ecs/config.tf
@@ -1,10 +1,4 @@
-terraform {
-  required_version = "~> 0.11.0"
-}
-
 provider "aws" {
-  version = "~> 1.3.1"
-
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
